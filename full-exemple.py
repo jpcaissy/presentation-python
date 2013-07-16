d23e09337e67dc1c61d8fc84c7f0c7c756fb194b
Jean-Philippe Caissy <jpcaissy@piji.ca>
2013-07-16 19:20:47 -0400

    Typo dans presentation

--- full-exemple.py
+++ full-exemple.py
@@ -1,11 +1,7 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.superadmin import Admin
-from flask.ext.mongorest import MongoRest
-from flask.ext.mongorest.views import ResourceView
-from flask.ext.mongorest.resources import Resource
-from flask.ext.mongorest import operators as ops
-from flask.ext.mongorest import methods
+
 
 from mongoengine import (
     Document, EmbeddedDocument, EmbeddedDocumentField,
@@ -25,9 +21,6 @@ class Blog(Document):
     auteur = EmbeddedDocumentField(Author)
     comments = ListField(EmbeddedDocumentField(Comment))
 
-class BlogResource(Resource):
-    document = Blog
-
 app = Flask("Presentation Python")
 
 app.config['MONGODB_SETTINGS'] = {'DB': 'presentation_python'}
@@ -37,11 +30,6 @@ db = MongoEngine(app)
 admin = Admin(app)
 admin.register(Blog)
 
-
-@api.register(name='blogs', url='/blog/')
-class BlogView(ResourceView):
-    resource = PostResource
-    methods = [methods.Create, methods.Update, methods.Fetch, methods.List]
 
 if __name__ == "__main__":
     app.run()
